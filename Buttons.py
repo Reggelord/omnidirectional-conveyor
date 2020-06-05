20b13d27681043938efaa1ef2d3bd46d9c041cef
test <bndamira@gmail.com>
2020-06-05 12:30:47 +0200

    Resolve #4 #10

---
+++ Buttons.py
@@ -0,0 +1,81 @@
+import pygame_gui, pygame
+
+class Buttons():
+    def __init__(self,window,clock,buttons_location = [[150,40],10],buttons_size = [5,280],clock_speed=30):
+
+        self.window = window
+        self.UI = pygame_gui.UIManager((800, 600), 'data/themes/quick_theme.json')
+        self.buttons_names = ['Start/Stop','Gen_path_points','Reset','Draw path']
+        self.buttons={} #dictionary of buttons objects
+        self.setup_buttons(buttons_location[0],buttons_location[1],buttons_size)
+
+        self.clock = clock
+        self.clock_speed = clock_speed
+        #Buttons state
+        self.is_sim_run = 0 #is simulaion running, 'Start/Stop' buttons
+        self.is_reset = 0
+        self.gen_path_points = 0 #if generating new path points, 'Gen_path_points' button
+        self.drawing_path = 0 #if path is drawn, 'Draw path' button
+        
+
+    def setup_buttons(self,size,space,position):
+        '''
+        Function for setting up buttons\n
+        Input: \n
+            size - size of buttons [x,y]\n space - space between buttons \nposition - position of buttons [x,y]
+        Output: None 
+        '''
+        #Create buttons object and add them to 'buttons' dictionary
+        for i,button_name in enumerate(self.buttons_names):
+            location = pygame.Rect((position[0], position[1]+i*(space+size[1])), (size[0], size[1]))
+            self.buttons[button_name] = pygame_gui.elements.UIButton(location,button_name,self.UI)
+
+
+    def button_clicked(self,event):
+        '''
+        Function that handles button clicks\n
+        Input: \n
+            event - pygame event object
+        Output: None
+        '''
+        if (event.type == pygame.USEREVENT and event.user_type == pygame_gui.UI_BUTTON_PRESSED): 
+            ## Start/Stop ##
+            if event.ui_element == self.buttons['Start/Stop']:
+                chenage_text = ["Start","Stop"]
+                self.is_sim_run = not self.is_sim_run
+                self.buttons['Start/Stop'].set_text(chenage_text[self.is_sim_run])
+
+            ## Path Gen ##
+            if event.ui_element == self.buttons['Gen_path_points']:
+                self.gen_path_points = not self.gen_path_points
+
+            ## Draw path ##
+            if event.ui_element == self.buttons['Draw path']:
+                try:
+                    self.box_path.generate_path(1)
+                    self.drawing_path = not self.drawing_path
+                
+                except: 
+                    print("Could not generate path")
+
+            ## Reset ##
+            if event.ui_element == self.buttons['Reset']:
+                
+                self.is_reset = 1
+                self.is_sim_run = 0
+            else:
+                self.is_reset = 0    
+        
+    def update_events(self,event):
+        self.button_clicked(event)
+        self.UI.process_events(event)
+        
+    
+    def draw_buttons(self):
+        self.UI.update(self.clock.tick(self.clock_speed))
+        self.UI.draw_ui(self.window)
+
+    def get_simulaion_state(self):
+        
+
+        return self.is_sim_run, self.is_reset
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from pygame.locals import*
 from Box import Box
 from Platform import Platform
+from Buttons import Buttons
 
 class Main_window():
     def __init__(self,height,width,default = 1):
@@ -21,22 +22,21 @@ class Main_window():
         self.font = pygame.font.SysFont("arial", 10,bold = False)
         self.screen = pygame.display.set_mode((width,height))
         self.clock = pygame.time.Clock()
-        #Setup GUI
-        self.UI = pygame_gui.UIManager((800, 600), 'data/themes/quick_theme.json')
-        self.buttons_names = ['Start/Stop','Gen_path_points','Reset','Draw path']
-        self.buttons={} #dictionary of buttons objects
-        self.setup_buttons([150,40],10,[5,280])
         #Buttons state
-        self.is_sim_run = 0 #is simulaion running, 'Start/Stop' buttons
-        self.gen_path_points = 0 #if generating new path points, 'Gen_path_points' button
-        self.drawing_path = 0 #if path is drawn, 'Draw path' button
+        self.is_sim_run = 0
+        self.is_reset = 0
         #Objects 
+        self.buttons = Buttons(self.screen,self.clock)
         self.box = None #Box object
         self.platform = None #Platform object
         self.box_path = None #Box path object
         self.controler = None #Platform controler object
         if default:
             self.setup_objects_default()
+
+    def reset_sim(self):
+        self.setup_objects_default()
+
 
     def setup_objects_default(self):
         '''
@@ -45,40 +45,7 @@ class Main_window():
         Output: None  
         '''
         self.platform = Platform((200,200),(10,10),50,self.screen,self.font)
-        self.box = Box((40,80),[450,700,0],self.screen)      
-        
-
-
-    def setup_buttons(self,size,space,position):
-        '''
-        Function for setting up buttons\n
-        Input: \n
-            size - size of buttons [x,y]\n space - space between buttons \nposition - position of buttons [x,y]
-        Output: None 
-        '''
-        #Create buttons object and add them to 'buttons' dictionary
-        for i,button_name in enumerate(self.buttons_names):
-            location = pygame.Rect((position[0], position[1]+i*(space+size[1])), (size[0], size[1]))
-            self.buttons[button_name] = pygame_gui.elements.UIButton(location,button_name,self.UI)
-
-
-    def button_clicked(self,event):
-        '''
-        Function that handles button clicks\n
-        Input: \n
-            event - pygame event object
-        Output: None
-        '''
-        if (event.type == pygame.USEREVENT and event.user_type == pygame_gui.UI_BUTTON_PRESSED): 
-            ## Start/Stop ##
-            if event.ui_element == self.buttons['Start/Stop']:
-                chenage_text = ["Start","Stop"]
-                self.is_sim_run = not self.is_sim_run
-                self.buttons['Start/Stop'].set_text(chenage_text[self.is_sim_run])
-
-            ######## Must add other buttons ######
-        
-       
+        self.box = Box((40,80),[450,700,0],self.screen)       
 
     def run_sim(self):
         '''
@@ -86,22 +53,27 @@ class Main_window():
         Input: None\n
         Output: None
         '''
-        try:
-            #self.platform.setup_all_cells_vel([0.5,-1,0.2]) #for testing purpose
-            self.platform.check_contact(self.box)
-            contact_list = self.platform.get_contact_cell_list()
-            if contact_list:
-                self.platform.update_cells_vel(contact_list,[0.5,-1,0.2])
-                
-                self.box.move_obj(self.platform.get_cell_vel([contact_list[0].get_cell_number()]))
-                
+        #Update simulation state
+        self.is_sim_run, self.is_reset = self.buttons.get_simulaion_state()
+
+        if self.is_reset:
+            self.reset_sim()
+
+        if self.is_sim_run:
+            try:
+                #self.platform.setup_all_cells_vel([0.5,-1,0.2]) 
+                self.platform.check_contact(self.box)
+                contact_list = self.platform.get_contact_cell_list()
+                if contact_list:
+                    self.platform.update_cells_vel(contact_list,[0.5,-1,0.2]) #for testing purpose
+                    
+                    self.box.move_obj(self.platform.get_cell_vel([contact_list[0].get_cell_number()]))
+
+            except:
+                self.is_sim_run = 0
+                self.buttons['Start/Stop'].set_text(chenage_text[self.is_sim_run])
+                print('Cant run sim')
             
-        
-        except:
-            self.is_sim_run = 0
-            self.buttons['Start/Stop'].set_text(chenage_text[self.is_sim_run])
-            print('Cant run sim')
-        
         self.draw_update_objects()
 
     def draw_update_objects(self):
@@ -113,28 +85,26 @@ class Main_window():
         self.screen.fill((0,0,0))
         self.platform.draw_platform(1)
         self.box.draw_box()
-        self.UI.update(self.clock.tick(10))
-        self.UI.draw_ui(self.screen)
+        self.buttons.draw_buttons()
+        
         
 
 def main():
 
     main_window = Main_window(900,800)
-
     pygame.display.update()
     while True:
 
         for events in pygame.event.get():
-            if events.type == QUIT:
+            main_window.buttons.update_events(events)
 
+            if events.type == QUIT:
                 sys.exit(0)
             
-            main_window.UI.process_events(events)
 
-
+        
 
         main_window.run_sim()
         pygame.display.update()
         
-
 main()
